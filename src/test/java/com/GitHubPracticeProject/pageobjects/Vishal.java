3a639c5d74636ef188a9ed5ac69526c7f525c162
Vishal <vishalambekar123@gmail.com>
2022-04-30 15:54:49 +0530

    hi nikhilllll

--- src/test/java/com/GitHubPracticeProject/pageobjects/Vishal.java
+++ src/test/java/com/GitHubPracticeProject/pageobjects/Vishal.java
@@ -3,7 +3,7 @@ package com.GitHubPracticeProject.pageobjects;
 public class Vishal {
 
 	public static void main(String[] args) {
-		int num = 257;
+		int num = 23;
 		int count = 0;
 
 		if (num > 1) {
