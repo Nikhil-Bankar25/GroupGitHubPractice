f23595ff655f4fdbf2ff68187f6acf0a271d1333
Vishal <vishalambekar123@gmail.com>
2022-04-30 15:45:14 +0530

    Hi Nikhil , change in values.

--- src/test/java/com/GitHubPracticeProject/pageobjects/Vishal.java
+++ src/test/java/com/GitHubPracticeProject/pageobjects/Vishal.java
@@ -3,7 +3,7 @@ package com.GitHubPracticeProject.pageobjects;
 public class Vishal {
 
 	public static void main(String[] args) {
-		int num = 101;
+		int num = 257;
 		int count = 0;
 
 		if (num > 1) {
