06579892de22d48eb58d11845a8e2bccb99db4c1
lenovo <lenovo@LAPTOP-1B3V1LJB>
2022-04-30 14:47:03 +0530

    Adding project to the git hub.

---
+++ src/test/java/com/GitHubPracticeProject/pageobjects/NIkhil.java
@@ -0,0 +1,10 @@
+package com.GitHubPracticeProject.pageobjects;
+
+public class NIkhil {
+
+	public static void main(String[] args) {
+		System.out.println("Nikhil creadted this class...");
+
+	}
+
+}
---
+++ src/test/java/com/GitHubPracticeProject/testcases/NIkhil.java
@@ -0,0 +1,10 @@
+package com.GitHubPracticeProject.testcases;
+
+public class NIkhil {
+
+	public static void main(String[] args) {
+		System.out.println("Nikhil creadted this class...");
+
+	}
+
+}
